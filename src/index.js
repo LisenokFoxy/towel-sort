70d9d955faab8f62820997aec75075752a9c113f
Tatyana Pashintseva <Tatyana.Pashintseva@kcell.kz>
2020-03-19 12:16:46 +0600

    fix: change logic

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return matrix.flat(Infinity);
+module.exports = function towelSort(matrix) {
+  let new_array=[];
+  if (matrix == null) return new_array;
+  for (let i=0; i<matrix.length; i++) {
+    if (i%2 == 0){
+        new_array=new_array.concat(matrix[i]);
+    } else {
+      new_array=new_array.concat(matrix[i].reverse());
+    }
+  }
+  return new_array;
 }
